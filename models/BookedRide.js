55d0f4a6471c8206366a8029fed4a2cc646c50f1
Venkata Jagapathi Babu K <158805619+NANI090205@users.noreply.github.com>
2026-05-29 12:51:10 +0530

    Refactor BookedRide schema with required fields

--- models/BookedRide.js
+++ models/BookedRide.js
@@ -1,17 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bookedRideSchema = new mongoose.Schema({
-  rideId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ride', required: true },
-  bookedBy: String,
-  bookedByCode: String,
-  publishedBy: String,
-  source: String,
-  destination: String,
-  date: String,
-  time: String,
-  price: Number,
-  seatsBooked: Number,
-  totalPrice: Number
+const bookedRideSchema = new mongoose.Schema(
+{
+    rideId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ride",
+        required: true
+    },
+
+    bookedBy: {
+        type: String,
+        required: true
+    },
+
+    bookedByCode: {
+        type: String,
+        required: true
+    },
+
+    publishedBy: {
+        type: String,
+        required: true
+    },
+
+    source: {
+        type: String,
+        required: true
+    },
+
+    destination: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: String,
+        required: true
+    },
+
+    time: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    seatsBooked: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true
+    }
+},
+{
+    timestamps: true
 });
 
-module.exports = mongoose.model('BookedRide', bookedRideSchema);
+module.exports = mongoose.model(
+    "BookedRide",
+    bookedRideSchema
+);
